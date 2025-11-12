dbbf56e370af37b8084c63851ddbb061866b5bae
Domirando <vohidjonovnamaftuna@gmail.com>
2025-11-12 15:41:48 +0500

    feat: impl and option sample code

--- a11.rs
+++ a11.rs
@@ -9,4 +9,23 @@
 // * Create a function to display the quantity, with the struct as a parameter
 // * Create a function to display the id number, with the struct as a parameter
 
-fn main() {}
+struct Grocery {
+    id: i32,
+    quantity: i32
+}
+
+fn display_quantity(grocery: &Grocery){
+    println!("{}", grocery.quantity);
+}
+fn display_id(grocery: &Grocery){
+    println!("{}", grocery.id);
+}
+
+fn main() {
+let grocery = Grocery {
+    id: 1,
+    quantity: 4
+};
+display_id(&grocery);
+display_quantity(&grocery);
+}
--- a12.rs
+++ a12.rs
@@ -10,4 +10,24 @@
 // * Implement functionality on the box struct to create a new box
 // * Implement functionality on the box struct to print the characteristics
 
-fn main() {}
+struct ShippingBox{
+    dimensions: i32,
+    weight: f32,
+    color: String
+}
+
+impl ShippingBox {
+    fn print_box(&self){
+        println!("dimensions: {}, weight: {}, color: {}", self.dimensions, self.weight, self.color);
+    }
+}
+
+
+fn main() {
+let s_box = ShippingBox {
+    dimensions: 3,
+    weight: 34.5,
+    color: "red".to_string()
+};
+s_box.print_box();
+}
