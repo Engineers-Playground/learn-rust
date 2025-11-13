c8f06eb0b78e2d2341b6e16b557c73c55d84a801
Domirando <vohidjonovnamaftuna@gmail.com>
2025-11-14 02:24:27 +0500

    add: vec sample code

--- a15.rs
+++ a15.rs
@@ -10,5 +10,17 @@
 // * Use an enum for the tickets with data associated with each variant
 // * Create one of each ticket and place into a vector
 // * Use a match expression while iterating the vector to print the ticket info
+enum Ticket {
+    Backstage(i32, String),
+    Vip(i32, String),
+    Standard(i32)
+}
 
-fn main() {}
+fn main() {
+let t: Vec<Ticket> = vec![Ticket::Backstage(31, "owner".to_owned()), Ticket::Standard(23), Ticket::Vip(12, "owner2".to_owned())]; 
+for i in t{
+match i {
+    Ticket::Backstage(price, holder) => println!("backstage: holder: {:?}, price: {:?}", holder, price),
+ Ticket::Vip(price, holder) => println!("vip: holder: {:?}, price: {:?}", holder, price),
+Ticket::Standard(price) => println!("standard: price: {:?}", price)
+}}}
