849381960880c12ae7c481c07ac550446a2e56a1
Domirando <vohidjonovnamaftuna@gmail.com>
2025-11-15 16:52:25 +0500

    feat: std library doc a17

--- a17.rs
+++ a17.rs
@@ -12,4 +12,7 @@
 //   to uppercase and lowercase
 //   * Try searching for: to_uppercase, to_lowercase
 
-fn main() {}
+fn main() {
+let s = "hello World";
+println!("uppercase: {}, lowecase: {}", s.to_uppercase(), s.to_lowercase()); 
+}
